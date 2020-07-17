6cd5d4849bbd6c95e528e32e247f8f9788b167c5
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-07-16 23:22:19 -0700

    Added single traversal and cleaned up some code.

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-from util import Stack, Queue, Graph
+from util import Graph
 
 import random
 from ast import literal_eval
@@ -30,27 +30,51 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
-# def start():
-#     graph = Graph()
-#     # CACHE ALL ROOMS WITH NEIGHBOR ID AS KEY AND DIRECTION AS VALUE
-#     dft_rooms = graph.dft(player.current_room)
-#     # MAKE LIST OF ROOMS
-#     rooms = [room for room in dft_rooms]
-#     print(dft_rooms)
-#     print("HOW MANY ROOMS", len(dft_rooms.keys()), "********")
-#     print(rooms)
-#     print("HOW MANY ROOMS", len(rooms), "********")
-#     # FIND SHORTEST PATH TO EACH ROOM
-#     path = graph.bfs(rooms[0], rooms[1])
-#     print("SHORTEST", path, "PATH")
-#     # CHECK NEIGHBORING ROOMS OF PATH AT INDEX 0
-#     neighbors = dft_rooms[path[0]]
-#     print("NEIGHBORS OF", neighbors, "SHORTEST AT INDEX 0")
-#     # FIND THE DIRECTION VALUE OF THE ROOM AT INDEX 1 SO IT CAN BE APPENDED TO TRAVERSAL PATH
-#     value = neighbors[path[1]]
-#     print("VALUE OF", value, "SHORTEST AT INDEX 1 IN NEIGHBORS OF SHORTEST AT INDEX 0")
+def start():
+    graph = Graph()
+    # CACHE ALL ROOMS WITH NEIGHBOR ID AS KEY AND DIRECTION AS VALUE
+    dft_rooms = graph.dft(player.current_room)
+    # MAKE LIST OF ROOMS
+    rooms = [room for room in dft_rooms]
+    print(dft_rooms)
+    print("HOW MANY ROOMS", len(dft_rooms.keys()), "********")
+    print(rooms)
+    print("HOW MANY ROOMS", len(rooms), "********")
+    # FIND SHORTEST PATH TO EACH ROOM
+    path = graph.bfs(rooms[0], rooms[1])
+    print("SHORTEST", path, "PATH")
+    # CHECK NEIGHBORING ROOMS OF PATH AT INDEX 0
+    neighbors = dft_rooms[path[0]]
+    print("NEIGHBORS OF", neighbors, "SHORTEST AT INDEX 0")
+    # FIND THE DIRECTION VALUE OF THE ROOM AT INDEX 1 SO IT CAN BE APPENDED TO TRAVERSAL PATH
+    value = neighbors[path[1]]
+    print("VALUE OF", value, "SHORTEST AT INDEX 1 IN NEIGHBORS OF SHORTEST AT INDEX 0")
 
 # start()
+
+
+def single_traverse():
+    graph = Graph()
+    visited = set()
+    mapped_rooms = graph.dft(player.current_room)
+    rooms = [room for room in mapped_rooms]
+
+    while len(visited) < len(room_graph) - 1:
+        # print(rooms[0])
+        path = graph.bfs(rooms[0], rooms[1])
+
+        while len(path) > 1:
+            cur_room = path[0]
+            adj_room = path[1]
+            if adj_room in mapped_rooms[cur_room]:
+                traversal_path.append(mapped_rooms[cur_room][adj_room])
+            path.remove(cur_room)
+        rooms.remove(rooms[0])
+        visited.add(rooms[0])
+    
+
+single_traverse()
+
 
 def traverse():
     graph = Graph()
@@ -80,19 +104,16 @@ def get_that_960():
     traversal_path = traverse()
     print(f"TESTS PASSED: {len(traversal_path)} moves")
 
-    if len(traversal_path) < 960:
-        print("That's a three!")
-
     old_length = len(traversal_path)
     # print(old_length)
 
-    while len(traversal_path) > 950:
+    while len(traversal_path) > 957:
         traversal_path = traverse()
         if len(traversal_path) < old_length:
             print(f"TESTS PASSED: {len(traversal_path)} moves")
             old_length = len(traversal_path)
 
-get_that_960()
+# get_that_960()
 
 # traverse()
 # print("TRAVERSAL PATH \n", traversal_path)
@@ -117,12 +138,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
